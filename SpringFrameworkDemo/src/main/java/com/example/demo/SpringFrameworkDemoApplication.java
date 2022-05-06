0a9602b61c435572ba9ae496688e9585bc6efffa
umang garg <umang.garg@zensar.com>
2022-05-06 16:28:22 +0530

    hello

--- SpringFrameworkDemo/src/main/java/com/example/demo/SpringFrameworkDemoApplication.java
+++ SpringFrameworkDemo/src/main/java/com/example/demo/SpringFrameworkDemoApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringFrameworkDemoApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(SpringFrameworkDemoApplication.class, args);
+		System.out.println("hello");
 	}
 
 }
